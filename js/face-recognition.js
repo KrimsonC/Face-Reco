6dadfb5ec0bc2717186d4eff5bc418f000b7bd96
Panos Karypiadis <pan.karyp@gmail.com>
2018-10-07 20:47:47 +0300

    Initial commit

---
+++ js/face-recognition.js
@@ -0,0 +1,17 @@
+$('#img').faceDetection({
+    complete: function (faces) {
+
+        console.log(faces);
+
+        //if (faces[0].confidence === -2.99391947 ) {
+        //    console.log('SUCESS');
+        //}
+
+        $div = $("<div>", {"class": "face-box"});
+        $div.css('top', faces[0].positionY);
+        $div.css('left', faces[0].positionX);
+        $div.css('width', faces[0].width);
+        $div.css('height', faces[0].height);
+        $("#wrapper").append($div);
+    }
+});
